d3364750d9e1ca11cca0d9f21001b73fe3e83166
Corentin Gitton <corentin.gitton@gmail.com>
2017-12-03 17:53:14 +0000

    :bug: Completed day 1 (part 2)

--- 2017/day01/script.py
+++ 2017/day01/script.py
@@ -2,9 +2,11 @@
 # coding: utf8
 
 
-def sum_captcha(captcha):
+def sum_captcha_part1(captcha):
     result = 0
     length = len(captcha)
+
+    # Adding the first value at the end to make it kind if circular
     captcha += captcha[0]
 
     for i in range(0, length):
@@ -14,8 +16,25 @@ def sum_captcha(captcha):
     return result
 
 
+def sum_captcha_part2(captcha):
+    result = 0
+    length = len(captcha)
+
+    offset = int(length/2)
+
+    # Doubling the size to make it kind if circular
+    captcha += captcha
+
+    for i in range(0, length):
+        if captcha[i] == captcha[i+offset]:
+            result += int(captcha[i])
+
+    return result
+
+
 if __name__ == '__main__':
     with open('input.txt') as f:
         input_data = str(f.read()).strip()
 
-    print(sum_captcha(input_data))
+    print('Part 1:', sum_captcha_part1(input_data))
+    print('Part 2:', sum_captcha_part2(input_data))
--- 2017/day01/tests.py
+++ 2017/day01/tests.py
@@ -2,21 +2,34 @@
 # coding: utf8
 
 import unittest
-from script import sum_captcha
+from script import sum_captcha_part1, sum_captcha_part2
 
-cases = [
+cases_part1 = [
     ('1122', 3),
     ('1111', 4),
     ('1234', 0),
     ('91212129', 9),
 ]
 
+cases_part2 = [
+    ('1212', 6),
+    ('1221', 0),
+    ('123425', 4),
+    ('123123', 12),
+    ('12131415', 4),
+]
+
 
 class TestScript(unittest.TestCase):
 
-    def test_sum_captcha(self):
-        for input_value, expected in cases:
-            result = sum_captcha(input_value)
+    def test_sum_captcha_part1(self):
+        for input_value, expected in cases_part1:
+            result = sum_captcha_part1(input_value)
+            self.assertEqual(result, expected)
+
+    def test_sum_captcha_part2(self):
+        for input_value, expected in cases_part2:
+            result = sum_captcha_part2(input_value)
             self.assertEqual(result, expected)
 
 
