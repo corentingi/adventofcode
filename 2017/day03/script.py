c57e8f5d9c0c8d8b54bff8ca4b0d63abaf361adc
Corentin Gitton <corentin.gitton@gmail.com>
2017-12-04 11:09:34 +0000

    :christmas_tree: Completed day 3 (part 2)

--- 2017/day03/script.py
+++ 2017/day03/script.py
@@ -2,6 +2,7 @@
 # coding: utf8
 
 import math
+from itertools import count
 
 
 # Value: x
@@ -9,7 +10,7 @@ import math
 # range: n as (2n+1)^2 <= x
 #     so n = int( (sqrt(x) - 1)/2 )
 # offset: o = abs( n - mod(x - 1, 2n) )
-def manhattan_steps(value):
+def manhattan_steps_part1(value):
     n = math.ceil((math.sqrt(value) - 1)/2)
 
     # Special case
@@ -21,5 +22,24 @@ def manhattan_steps(value):
         return distance
 
 
+# Uses solution from https://www.reddit.com/r/adventofcode/comments/7h7ufl/2017_day_3_solutions/dqox0fv/?st=jas39005&sh=95183492
+def sum_spiral():
+    a, i, j = {(0,0) : 1}, 0, 0
+    sn = lambda i,j: sum(a.get((k,l), 0) for k in range(i-1,i+2)
+                                         for l in range(j-1,j+2))
+    for s in count(1, 2):
+        for _ in range(s):   i += 1; a[i,j] = sn(i,j); yield a[i,j]
+        for _ in range(s):   j -= 1; a[i,j] = sn(i,j); yield a[i,j]
+        for _ in range(s+1): i -= 1; a[i,j] = sn(i,j); yield a[i,j]
+        for _ in range(s+1): j += 1; a[i,j] = sn(i,j); yield a[i,j]
+
+
+def manhattan_steps_part2(value):
+    for x in sum_spiral():
+        if x > value:
+            return x
+
+
 if __name__ == '__main__':
-    print(manhattan_steps(265149))
+    print('Part1:', manhattan_steps_part1(265149))
+    print('Part2:', manhattan_steps_part2(265149))
--- 2017/day03/tests.py
+++ 2017/day03/tests.py
@@ -2,9 +2,9 @@
 # coding: utf8
 
 import unittest
-from script import manhattan_steps
+from script import manhattan_steps_part1, manhattan_steps_part2
 
-cases = [
+cases_part1 = [
     (1, 0),
     (12, 3),
     (23, 2),
@@ -14,9 +14,9 @@ cases = [
 
 class TestScript(unittest.TestCase):
 
-    def test_manhattan_steps(self):
-        for input_value, expected in cases:
-            result = manhattan_steps(input_value)
+    def test_manhattan_steps_part1(self):
+        for input_value, expected in cases_part1:
+            result = manhattan_steps_part1(input_value)
             self.assertEqual(result, expected)
 
 
